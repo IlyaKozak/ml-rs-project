ce824fbdf97a3d174dc299869db413629d506942
Ilya Kozak <kozak@tut.by>
2022-05-05 11:30:34 +0300

    chore: add sklearn as dependency

--- src/forestml/train.py
+++ src/forestml/train.py
@@ -2,6 +2,9 @@ from pathlib import Path
 
 import click
 import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 
 @click.command()
